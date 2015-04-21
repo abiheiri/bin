a4cfd021119c7991ce75855d10554a182cbffcff
Al Biheiri <al@forgottheaddress.com>
2015-04-21 14:21:02 -0700

    extract mime attachments

---
+++ extract_mime.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+note () {
+	cat <<EOF
+	This script will extract the MIME messages so that you can feed them to spamassassin.
+	Make a backup of the files in case something breaks!
+
+	Usage $0 /path/to/emails [is-spam|is-notspam]
+
+	examples:
+       	$0 /tmp/safe_folder ham
+	$0 /tmp/spam_folder spam
+EOF
+
+
+if [[ -d "$1" ]]; then
+	case in $1)
+		spam)
+			#find $1 -type f -name \*.emlx -exec sed -i -r -n -e '/Content-Disposition: inline/,${p}' {} \;
+			find $1 -type f -exec sed -i -r -n -e '/Content-Disposition: inline/,${p}' {} \;
+			;;
+		ham)
+			echo "not implemented"
+			;;
+	esac
+else
+	note
+fi
+
+
+cat<<EOF
+Now you can feed the data using below steps on an smtp server
+
+	sa-learn --nosync --spam $1
+	sa-learn --nosync --ham $1
+	sa-learn --sync
+EOF
