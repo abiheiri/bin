441723571a568bcd40ed50f88a5b569f7d9b5199
Al Biheiri <abiheiri@apple.com>
2019-03-25 18:19:07 -0700

    minor

--- sync2drive.sh
+++ sync2drive.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-rsync -va --delete ~/Dropbox/ /Volumes/Elements/share/backups/abiheiri/Dropbox/
+EXTERNAL="/Volumes/Elements"
 
-rsync -va --delete "/Users/abiheiri/Library/Mobile Documents/com~apple~CloudDocs/Notes/" /Volumes/Elements/share/backups/abiheiri/Notes/
-
-if [ $HOSTNAME = "WORD" ];
-then
-	echo "Add stuff here todo"
-	#sync photos
-	#sync music
-	#sync audio production
-fi
+rsync -va --delete "$HOME/Dropbox/" "$EXTERNAL/share/backups/abiheiri/Dropbox/"
+rsync -va --delete "$HOME/Pictures/Photos\ Library.photoslibrary/" "$EXTERNAL/share/backups/Photos\ Library.photoslibrary/"
+rsync -va --delete "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes/" $EXTERNAL/share/backups/abiheiri/Notes/
+rsync -va --delete "$HOME/Dropbox/music\ production/ $EXTERNAL/share/backups/audio-projects/music\ production/"
