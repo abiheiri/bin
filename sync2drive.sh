3962b2996e84c10772869c21d8fef7e8061bbe36
Al Biheiri <abiheiri@apple.com>
2019-03-25 19:04:53 -0700

    fixed the paths

--- sync2drive.sh
+++ sync2drive.sh
@@ -3,6 +3,6 @@
 EXTERNAL="/Volumes/Elements"
 
 rsync -va --delete "$HOME/Dropbox/" "$EXTERNAL/share/backups/abiheiri/Dropbox/"
-rsync -va --delete "$HOME/Pictures/Photos\ Library.photoslibrary/" "$EXTERNAL/share/backups/Photos\ Library.photoslibrary/"
+rsync -va --delete "$HOME/Pictures/Photos Library.photoslibrary/" "$EXTERNAL/share/backups/Photos Library.photoslibrary/"
 rsync -va --delete "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes/" $EXTERNAL/share/backups/abiheiri/Notes/
-rsync -va --delete "$HOME/Dropbox/music\ production/ $EXTERNAL/share/backups/audio-projects/music\ production/"
+rsync -va --delete "$HOME/Dropbox/music production/" "$EXTERNAL/share/backups/audio-projects/music production/"
