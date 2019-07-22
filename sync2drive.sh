ddfbec6a7b505d48e7880f3388c037e1c2efc097
Al Biheiri <abiheiri@apple.com>
2019-07-22 09:46:38 -0700

    ignore dsstore

--- sync2drive.sh
+++ sync2drive.sh
@@ -3,6 +3,8 @@
 EXTERNAL="/Volumes/Elements"
 
 rsync -va --delete "$HOME/Dropbox/" "$EXTERNAL/share/backups/abiheiri/Dropbox/"
-rsync -va --delete "$HOME/Pictures/Photos Library.photoslibrary/" "$EXTERNAL/share/backups/Photos Library.photoslibrary/"
 rsync -va --delete "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes/" $EXTERNAL/share/backups/abiheiri/Notes/
 rsync -va --delete "$HOME/Dropbox/music production/" "$EXTERNAL/share/backups/audio-projects/music production/"
+
+#Dont delete remove data
+rsync -va "$HOME/Pictures/AutoImport/" "$EXTERNAL/share/backups/backup-pics-videos/AutoImport/"
