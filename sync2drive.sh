ec741c8328059009e8b24e451f98e08b33193316
Al Biheiri <abiheiri@apple.com>
2019-08-12 17:05:03 -0700

    added chefautomate

--- sync2drive.sh
+++ sync2drive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-EXTERNAL="/Volumes/Elements"
+EXTERNAL="/Volumes/SSD"
 
 rsync -va --delete "$HOME/Dropbox/" "$EXTERNAL/share/backups/abiheiri/Dropbox/"
-rsync -va --delete "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Notes/" $EXTERNAL/share/backups/abiheiri/Notes/
 rsync -va --delete "$HOME/Dropbox/music production/" "$EXTERNAL/share/backups/audio-projects/music production/"
+rsync -va --delete "$HOME/Documents/" "$EXTERNAL/share/backups/abiheiri/twofah docs/"
 
 #Dont delete remove data
 rsync -va "$HOME/Pictures/AutoImport/" "$EXTERNAL/share/backups/backup-pics-videos/AutoImport/"
