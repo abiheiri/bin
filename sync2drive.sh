a4e8786b1048f7b05230c0565d1aad23ad938a79
Al Biheiri <abiheiri@apple.com>
2019-09-06 11:28:35 -0700

    fix typo

--- sync2drive.sh
+++ sync2drive.sh
@@ -5,7 +5,7 @@ EXTERNAL="/Volumes/SSD"
 rsync -va --delete "$HOME/Dropbox/" "$EXTERNAL/share/backups/abiheiri/Dropbox/"
 rsync -va --delete "$HOME/Dropbox/music production/" "$EXTERNAL/share/backups/audio-projects/music production/"
 
-if [ $(hostname) = "Als-MacBook.local" ];
+if [ $(hostname) = "Als-Macbook.local" ];
 then
 	rsync -va --delete "$HOME/Documents/" "$EXTERNAL/share/backups/abiheiri/twofah docs/"
 fi
